92f10b0c58302e009172e1e4fbfe3e2d668abb09
ashutoshbilla <ashutoshbilla@macbook.local>
2017-08-27 07:17:23 +0530

    Initial Commit

---
+++ Gesture Recognizers/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gesture Recognizers
+//
+//  Created by Ashutosh Billa on 27/08/17.
+//  Copyright © 2017 Ashutosh Billa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
